0680f007a9442b6428d096d30c6acca934396f0d
Josek-dev <xjosekx@gmail.com>
2020-08-05 18:16:37 +0200

    added login

--- login.php
+++ login.php
@@ -1,20 +1,21 @@
 <!DOCTYPE HTML>
 <html lang="pl">
 <head>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="admin.css">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TYTUŁ</title>
+    <title>Panel administracyjny</title>
 </head>
 <body>
-<div id="container">
     <header>
-        
+        <h1>Panel administracyjny</h1>
     </header>
-
-
-</div>
+    <?php
+        $login = "admin";
+        $password = "cc03e747a6afbbcbf8be7668acfebee5";
+        ?>
 </body>
 </html>
+
