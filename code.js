533d615ac81ee7ae760f29c830510f27aca6ff54
Josek-dev <xjosekx@gmail.com>
2020-08-06 20:19:13 +0200

    menu

--- code.js
+++ code.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
     $("#backspace" ).click(function() {
         window.location.href = "http://wprost.pl";
     });
+    $("#title" ).click(function() {
+        window.location.href = "http://historia.wprost.pl";
+    });
+    $("#subtitle" ).click(function() {
+        window.location.href = "http://historia.wprost.pl";
+    });
 });
