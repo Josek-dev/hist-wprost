d6b6c41534c5de8c831a2adae6911e920928f96d
Josek-dev <xjosekx@gmail.com>
2020-08-08 13:46:32 +0200

    ajax login

---
+++ login.js
@@ -0,0 +1,13 @@
+function loginAjax() {
+    let login = $("#name").val();
+    let password = $("#password").val();
+    const action = "login";
+
+    $("#name").empty(function() {
+        this.css("border-bottom", "2px #000 solid");
+        $("label:eq(0)").text
+    }
+}
+
+
+
