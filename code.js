235425d1b7f675244de03686b58eb8c9cbd9c3a1
Josek-dev <xjosekx@gmail.com>
2020-08-08 04:08:08 +0200

    flex

--- code.js
+++ code.js
@@ -8,4 +8,13 @@ $(document).ready(function() {
     $("#subtitle" ).click(function() {
         window.location.href = "http://historia.wprost.pl";
     });
+    $("#top-bar" ).click(function() {
+        window.location.href = "https://cyfrowy.wprost.pl/oferty";
+    });
+    $( "#menu-icon" ).click(function() {
+        $( "#overlay" ).toggle( "slow", function() {
+            $("#top-bar").css("margin-top", "0");
+            $("#top-bar>p").css("margin", "0");
+        });
+    });
 });
---
+++ menu.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById(
+        "overlayNavigation").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById(
+        "overlayNavigation").style.display = "none";
+}
